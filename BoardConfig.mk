490ae8f147e9fe5b5a7fe91b5576b22b79322db3
Razziell <razziell@ya.ru>
2018-02-01 22:48:36 +0300

    Boardconfig: upd DEXPREOPT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,10 +123,10 @@ ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 # Display
 MAX_VIRTUAL_DISPLAY_DIMENSION := 4096
