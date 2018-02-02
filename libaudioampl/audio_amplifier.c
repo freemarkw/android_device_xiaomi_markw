8213c5d9500c1a678ee1665053c78aa63474c67c
Anomalchik <vidslava@yandex.ru>
2018-02-02 21:24:44 +0300

    remove perflocks from amp and remove custom power hal

--- libaudioampl/audio_amplifier.c
+++ libaudioampl/audio_amplifier.c
@@ -34,32 +34,6 @@
 
 #define DEVICE_PATH "/sys/audio_amplifier/enable"
 
-#define MPCTLV3 1
-
-#include <performance.h>
-
-static void *handle = NULL;
-typedef int (*perf_lock_acquire_t)(int, int, int*, int);
-typedef int (*perf_lock_release_t)(int);
-
-static void *qcopt_handle;
-static perf_lock_acquire_t perf_lock_acq;
-static perf_lock_release_t perf_lock_rel;
-
-static char opt_lib_path[512] = {0};
-
-static int perf_lock_handle;
-static int perf_lock_opts[] = {
-  MIN_FREQ_BIG_CORE_0, 0x28b,
-  MIN_FREQ_LITTLE_CORE_0, 0x28b,
-};
-static int perf_lock_opts_size = 4;
-
-static unsigned long perf_lock_time = 0;
-
-int speaker_ref_count = 0;
-int lock_ref = 0;
-
 static int is_speaker(uint32_t snd_device) {
     int speaker = 0;
 
@@ -107,75 +81,6 @@ static inline int amplifier_disable() {
   return write_int(DEVICE_PATH, 0);
 }
 
-int audio_extn_perf_lock_init(void)
-{
-    int ret = 0;
-    if (qcopt_handle == NULL) {
-        if (property_get("ro.vendor.extension_library",
-                         opt_lib_path, NULL) <= 0) {
-            ALOGE("%s: Failed getting perf property \n", __func__);
-            ret = -EINVAL;
-            goto err;
-        }
-        if ((qcopt_handle = dlopen(opt_lib_path, RTLD_NOW)) == NULL) {
-            ALOGE("%s: Failed to open perf handle \n", __func__);
-            ret = -EINVAL;
-            goto err;
-        } else {
-            perf_lock_acq = (perf_lock_acquire_t)dlsym(qcopt_handle,
-                                                       "perf_lock_acq");
-            if (perf_lock_acq == NULL) {
-                ALOGE("%s: Perf lock Acquire NULL \n", __func__);
-                ret = -EINVAL;
-                goto err;
-            }
-            perf_lock_rel = (perf_lock_release_t)dlsym(qcopt_handle,
-                                                       "perf_lock_rel");
-            if (perf_lock_rel == NULL) {
-                ALOGE("%s: Perf lock Release NULL \n", __func__);
-                ret = -EINVAL;
-                goto err;
-            }
-            ALOGE("%s: Perf lock handles Success \n", __func__);
-        }
-    }
-err:
-    return ret;
-}
-
-void audio_extn_perf_lock_acquire(int *handle, int duration,
-                                 int *perf_lock_opts, int size)
-{
-
-    if (!perf_lock_opts || !size || !perf_lock_acq || !handle) {
-        ALOGE("%s: Incorrect params, Failed to acquire perf lock, err ",
-              __func__);
-        return;
-    }
-    //ALOGE("Acquiring perf lock");
-    /*
-     * Acquire performance lock for 1 sec during device path bringup.
-     * Lock will be released either after 1 sec or when perf_lock_release
-     * function is executed.
-     */
-    *handle = perf_lock_acq(*handle, duration, perf_lock_opts, size);
-    if (*handle <= 0)
-        ALOGE("%s: Failed to acquire perf lock, err: %d\n",
-              __func__, *handle);
-}
-
-void audio_extn_perf_lock_release(int *handle)
-{
-    if (perf_lock_rel && handle && (*handle > 0)) {
-        ALOGE("Releasing perf lock");
-        perf_lock_rel(*handle);
-        *handle = 0;
-    } else {
-        ALOGE("%s: Perf lock release error \n", __func__);
-    }
-}
-
-
 
 static int amp_set_input_devices(amplifier_device_t *device, uint32_t devices)
 {
@@ -184,39 +89,17 @@ static int amp_set_input_devices(amplifier_device_t *device, uint32_t devices)
 
 static int amp_set_output_devices(amplifier_device_t *device, uint32_t devices)
 {
-    /*ALOGE("amp_set_output_devices: %d", devices);
-    if (!is_speaker(devices)) {
-      speaker_ref_count = 0;
-      amplifier_disable();
-    }*/
     return 0;
 }
 
 static int amp_enable_output_devices(amplifier_device_t *device,
         uint32_t devices, bool enable)
 {
-    ALOGE("amp_enable_output_devices: %d, %d", devices, enable);
-    if (is_speaker(devices) && !enable) {
-      speaker_ref_count = 0;
-    }
     if (is_speaker(devices) && enable) {
       amplifier_enable();
     }
     if (is_speaker(devices) && !enable) {
       amplifier_disable();
-    }
-    if (enable) {
-      if (!lock_ref) {
-        audio_extn_perf_lock_acquire(&perf_lock_handle, 0,
-                                       perf_lock_opts,
-                                       perf_lock_opts_size);
-      }
-      lock_ref++;
-    } else {
-      lock_ref--;
-      if (!lock_ref) {
-        audio_extn_perf_lock_release(&perf_lock_handle);
-      }
     }
     return 0;
 }
@@ -235,15 +118,6 @@ static int amp_set_mode(amplifier_device_t *device, audio_mode_t mode)
 static int amp_output_stream_start(amplifier_device_t *device,
         struct audio_stream_out *stream, bool offload)
 {
-  /*  struct stream_out *out = (struct stream_out*) stream;
-    uint32_t devices = out->devices;
-    ALOGE("amp_output_stream_start: %d", devices);
-    if (is_speaker(devices)) {
-      if (!speaker_ref_count) {
-        amplifier_enable();
-      }
-      speaker_ref_count++;
-    }*/
     return 0;
 }
 
@@ -256,15 +130,6 @@ static int amp_input_stream_start(amplifier_device_t *device,
 static int amp_output_stream_standby(amplifier_device_t *device,
         struct audio_stream_out *stream)
 {
-    /*struct stream_out *out = (struct stream_out*) stream;
-    uint32_t devices = out->devices;
-    ALOGE("amp_output_stream_standby: %d", devices);
-    if (is_speaker(devices)) {
-      speaker_ref_count--;
-      if (speaker_ref_count == 0) {
-        amplifier_disable();
-      }
-    }*/
     return 0;
 }
 
@@ -302,8 +167,6 @@ static int amp_module_open(const hw_module_t *module, const char *name,
                 __func__, __LINE__);
         return -ENOMEM;
     }
-
-    audio_extn_perf_lock_init();
 
     amp_dev->common.tag = HARDWARE_DEVICE_TAG;
     amp_dev->common.module = (hw_module_t *) module;
