884f08c2dde83aa986a3986b6494ef996534d05e
Razziell <razziell@ya.ru>
2018-02-27 19:46:45 +0300

    RR

--- rr.mk
+++ rr.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/markw/full_markw.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_markw
+PRODUCT_NAME := rr_markw
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
