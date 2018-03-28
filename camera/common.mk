0758ca36ea1284bc243202e44b101a52db932111
=?UTF-8?q?Cosme=20Dom=C3=ADnguez=20D=C3=ADaz?= <cosme.ddiaz@gmail.com>
2018-03-28 19:38:22 +0200

    markw: camera: Remove deprecated kernel header dependency path.

--- camera/common.mk
+++ camera/common.mk
@@ -3,7 +3,7 @@ kernel_includes :=
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-    common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+    common_deps += INSTALLED_KERNEL_HEADERS
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 endif
 endif
