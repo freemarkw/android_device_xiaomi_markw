c1603d1d594d8b8be042bcc4267efbd7a7e197d7
Anomalchik <vidslava@yandex.ru>
2018-04-10 00:49:26 +0300

    markw: Fix vendor image mismatch * Needed after https://review.lineageos.org/#/c/208868/

--- elixir.mk
+++ elixir.mk
@@ -25,5 +25,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="Xiaomi/markw/markw:6.0.1/MMB29M/V9.2.3.0.MBEMIEK:user/release-keys" \
+    PRODUCT_NAME=markw \
     PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V9.2.3.0.MBEMIEK release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V9.2.3.0.MBEMIEK:user/release-keys
