e6f83080017f5a01fe02ff778a87fe106138731e
Bruno Martins <bgcngm@gmail.com>
2018-04-09 23:07:57 +0100

    markw: Drop USB HAL service
    * None of the Xiaomi 8953 devices have support for dual role type-C,
       therefore there's no need to build it.
    
    Change-Id: Ie5b135b8920ae255a2054a04cb7326c68abad0cd

--- device.mk
+++ device.mk
@@ -367,10 +367,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp
