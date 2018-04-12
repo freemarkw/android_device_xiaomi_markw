ff27ae03f10c77ab0dbb7dd49621caea5308f416
Razziell <razziell@ya.ru>
2018-04-12 19:09:34 +0300

    Enable CPU_INPUT_BOOST

--- rootdir/vendor/bin/init.qcom.post_boot.sh
+++ rootdir/vendor/bin/init.qcom.post_boot.sh
@@ -332,13 +332,20 @@ case "$target" in
                 # Governor settings
                 echo 1 > /sys/devices/system/cpu/cpu0/online
                 echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo "20000 1036800:40000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                echo "20000 1209600:40000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
                 echo 95 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
                 echo 1209600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo 0 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
                 echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                 echo 652800 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
                 echo 652800 > /sys/devices/system/cpu/cpufreq/interactive/screen_off_maxfreq
+
+                ### CPU_INPUT_BOOST
+                # Only boost power cores
+                echo "652800 1401600" > /sys/kernel/cpu_input_boost/ib_freqs
+                #Input boost duration
+                echo 400 > /sys/kernel/cpu_input_boost/ib_duration_ms
+                echo 1 > /sys/kernel/cpu_input_boost/enabled
 
 
                 # Don't put new tasks on the core which is 70% loaded
