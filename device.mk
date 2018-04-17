951b240d7fb3be30f20565ded4c12b269b9ed1f1
Razziell <razziell@ya.ru>
2018-04-17 17:06:04 +0300

    Add XiaomiParts (thx https://github.com/Hikari-no-Tenshi)

--- device.mk
+++ device.mk
@@ -370,5 +370,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# XiaomiParts
 PRODUCT_PACKAGES += \
     XiaomiParts
