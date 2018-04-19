75deaaaa732f9c61f852e2f84e163d4a45bf5b38
Anomalchik <vidslava@yandex.ru>
2018-04-19 15:39:27 +0300

    markw: vndk-sp: add EXTRA_VENDOR_LIBRARIES

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -3,7 +3,6 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.common@1.0 \
     android.hardware.renderscript@1.0 \
-    android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     libRSCpuRef \
     libRSDriver \
@@ -26,3 +25,11 @@ VNDK_SP_LIBRARIES := \
     libpng \
     libunwind \
     libutils \
+    libz
+
+EXTRA_VENDOR_LIBRARIES := \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    vendor.display.color@1.0 \
+    vendor.display.config@1.0 \
+    vendor.qti.hardware.iop@1.0
