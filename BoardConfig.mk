5090f2a4a6f03fe97973e35ca5741c1e8b43e4e9
Anomalchik <vidslava@yandex.ru>
2018-04-20 13:20:17 +0300

    include qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Wi-Fi
