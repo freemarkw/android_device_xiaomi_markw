f46f62fd2809484f87f45901b21a42294994c56c
Anomalchik <vidslava@yandex.ru>
2018-04-22 16:17:07 +0300

    markw: add ir service

--- device.mk
+++ device.mk
@@ -140,7 +140,8 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
