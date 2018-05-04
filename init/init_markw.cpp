80b0f06db1493cfe6d5f8f34c87f8c647afbe892
Alexandr <razziell@ya.ru>
2018-05-04 11:07:14 +0300

    Update init_markw.cpp

--- init/init_markw.cpp
+++ init/init_markw.cpp
@@ -39,6 +39,14 @@ char const *heapgrowthlimit;
 char const *heapsize;
 char const *heapminfree;
 char const *heapmaxfree;
+char const *texture_cache_size;
+char const *layer_cache_size;
+char const *shape_cache_size;
+char const *gradient_cache_size;
+char const *drop_shadow_cache_size;
+char const *small_cache_width;
+char const *small_cache_height;
+char const *large_cache_width;
 char const *large_cache_height;
 
 
@@ -86,15 +94,31 @@ void check_device()
         heapsize = "512m";
         heapminfree = "4m";
         heapmaxfree = "8m";
-	large_cache_height = "2048";
+        texture_cache_size="88";
+        layer_cache_size="58";
+        shape_cache_size="4";
+        gradient_cache_size="2";
+        drop_shadow_cache_size="8";
+        small_cache_width="2048";
+        small_cache_height="2048";
+        large_cache_width="4096";
+        large_cache_height = "2048";
     } else if (sys.totalram > 2048ull * 1024 * 1024) {
         // from - phone-xxhdpi-3072-dalvik-heap.mk
         heapstartsize = "8m";
         heapgrowthlimit = "288m";
         heapsize = "768m";
         heapminfree = "512k";
-	heapmaxfree = "8m";
-        large_cache_height = "1024";
+        heapmaxfree = "8m";
+        texture_cache_size="88";
+        layer_cache_size="58";
+        shape_cache_size="4";
+        gradient_cache_size="2";
+        drop_shadow_cache_size="8";
+        small_cache_width="2048";
+        small_cache_height="2048";
+        large_cache_width="4096";
+        large_cache_height = "4096";
     } else {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
         heapstartsize = "16m";
@@ -102,6 +126,13 @@ void check_device()
         heapsize = "512m";
         heapminfree = "2m";
         heapmaxfree = "8m";
+        texture_cache_size="72";
+        layer_cache_size="48";
+        gradient_cache_size="1";
+        drop_shadow_cache_size="6";
+        small_cache_width="1024";
+        small_cache_height="1024";
+        large_cache_width="2048";
         large_cache_height = "1024";
    }
 }
@@ -118,15 +149,15 @@ void vendor_load_properties()
     android::init::property_set("dalvik.vm.heapminfree", heapminfree);
     android::init::property_set("dalvik.vm.heapmaxfree", heapmaxfree);
 
-    android::init::property_set("ro.hwui.texture_cache_size", "72");
-    android::init::property_set("ro.hwui.layer_cache_size", "48");
+    android::init::property_set("ro.hwui.texture_cache_size", texture_cache_size);
+    android::init::property_set("ro.hwui.layer_cache_size", layer_cache_size);
     android::init::property_set("ro.hwui.r_buffer_cache_size", "8");
     android::init::property_set("ro.hwui.path_cache_size", "32");
-    android::init::property_set("ro.hwui.gradient_cache_size", "1");
-    android::init::property_set("ro.hwui.drop_shadow_cache_size", "6");
+    android::init::property_set("ro.hwui.gradient_cache_size", gradient_cache_size);
+    android::init::property_set("ro.hwui.drop_shadow_cache_size", drop_shadow_cache_size);
     android::init::property_set("ro.hwui.texture_cache_flushrate", "0.4");
-    android::init::property_set("ro.hwui.text_small_cache_width", "1024");
-    android::init::property_set("ro.hwui.text_small_cache_height", "1024");
-    android::init::property_set("ro.hwui.text_large_cache_width", "2048");
+    android::init::property_set("ro.hwui.text_small_cache_width", small_cache_width);
+    android::init::property_set("ro.hwui.text_small_cache_height", small_cache_height);
+    android::init::property_set("ro.hwui.text_large_cache_width", large_cache_width);
     android::init::property_set("ro.hwui.text_large_cache_height", large_cache_height);
 }
