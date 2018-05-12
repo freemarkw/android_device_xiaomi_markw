8b511069d5520228f9d486561af983ff5a9d93e2
Alexandr <razziell@yandex.by>
2018-05-12 21:59:54 +0300

    Initial Sepolicy (taken with mido)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,7 +222,8 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
