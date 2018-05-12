f03a0124857cdc52d5dc82d46080d14327ba9656
Alexandr <razziell@yandex.by>
2018-05-12 19:18:09 +0300

    Rename fingenprint service

--- device.mk
+++ device.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-custom
+    android.hardware.biometrics.fingerprint@2.0-service.xiaomi_markw
 
 # FM
 PRODUCT_PACKAGES += \
