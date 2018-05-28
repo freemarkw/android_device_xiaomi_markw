f2638ffd68b1222309fdcec4e22976c71631337a
Alexandr <razziell@ya.ru>
2018-05-28 16:34:43 +0300

    init: added missing override values for GSI

--- init/init_markw.cpp
+++ init/init_markw.cpp
@@ -158,6 +158,10 @@ void vendor_load_properties()
     check_device();
 
     // For GSI
+    property_override("ro.product.model", "Redmi 4 Prime");
+    property_override("ro.product.brand", "Xiaomi");
+    property_override("ro.product.name", "markw");
+    property_override("ro.product.device", "markw");
     property_override("ro.product.manufacturer", "Xiaomi");
     property_override("ro.build.product", "markw");
     property_override("ro.build.description", "markw-user 6.0.1 MMB29M V9.2.3.0.MBEMIEK release-keys");
