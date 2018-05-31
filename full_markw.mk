63025564e02bc43c0793621b4d59ce16c760156b
anomalchik <vidslava@yandex.ru>
2018-05-31 20:16:05 +0300

    markw: make: update copyrights and comments

--- full_markw.mk
+++ full_markw.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from mido device
+# Inherit from markw device
 $(call inherit-product, device/xiaomi/markw/device.mk)
 
 # Device identifier. This must come after all inclusions
