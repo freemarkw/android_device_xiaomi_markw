3385ef63526a22d7f05dd86868d446795a386d74
Anomalchik <vidslava@yandex.ru>
2018-06-18 11:33:48 +0300

    markw: Add OEM privileged app permissions

--- device.mk
+++ device.mk
@@ -298,6 +298,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Privileged app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-oem.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
