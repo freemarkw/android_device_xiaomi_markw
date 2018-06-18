430ce823d149eee78193e863668174c98c38e849
Alexandr <razziell@ya.ru>
2018-06-18 17:41:01 +0300

    Update vndk-sp-libs.mk

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,4 +1,5 @@
 VNDK_SP_LIBRARIES := \
+    android.hidl.base@1.0 \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.common@1.0 \
@@ -28,7 +29,6 @@ VNDK_SP_LIBRARIES := \
     libz
 
 EXTRA_VENDOR_LIBRARIES := \
-    android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
     vendor.display.color@1.0 \
     vendor.display.config@1.0 \
