bc19201594eb1036da29cfe795d5288a5b51e433
Razziell <razziell@ya.ru>
2018-06-19 23:30:36 +0300

    Add initial blank perf profile configs

--- device.mk
+++ device.mk
@@ -297,7 +297,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
+    $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Privileged app permissions
 PRODUCT_COPY_FILES += \
