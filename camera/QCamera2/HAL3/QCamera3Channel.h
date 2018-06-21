2b273028a3606395e6c78290391e46e2b8ed3452
Anomalchik <vidslava@yandex.ru>
2018-06-21 13:46:44 +0300

    markw: camera: last sync from my repo https://github.com/anomalchik/qcom_camera

--- camera/QCamera2/HAL3/QCamera3Channel.h
+++ camera/QCamera2/HAL3/QCamera3Channel.h
@@ -224,6 +224,8 @@ public:
 
     QCamera3PostProcessor m_postprocessor; // post processor
     void showDebugFPS(int32_t streamType);
+    bool isFwkInputBuffer(uint32_t resultFrameNumber);
+    int32_t releaseInputBuffer(uint32_t resultFrameNumber);
 
 protected:
     uint8_t mDebugFPS;
@@ -568,6 +570,8 @@ private:
     int32_t resetToCamPerfNormal(uint32_t frameNumber);
     android::List<OfflineBuffer> mOfflineBuffers;
     android::List<OfflineBuffer> mOfflineMetaBuffers;
+    Mutex mOfflineBuffersLock;
+    Mutex mOfflineMetaBuffersLock;
     int32_t mOfflineBuffersIndex;
     int32_t mOfflineMetaIndex;
     uint32_t mFrameLen;
