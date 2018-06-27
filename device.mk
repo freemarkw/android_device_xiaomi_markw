344223dedffc8e960f83578325971e40db1e33af
Mikhail Kshevetskiy <mikhail.kshevetskiy@gmail.com>
2018-06-27 16:14:25 +0300

    switch to bindered light hal

--- device.mk
+++ device.mk
@@ -252,9 +252,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    android.hardware.light@2.0-service.xiaomi_markw
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
