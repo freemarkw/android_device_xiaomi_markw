14983933bc45dc76321919b276f34b1010472ba4
Razziell <razziell@ya.ru>
2018-07-02 16:48:31 +0300

    markw: Remove btnvtool *init: cannot find '/vendor/bin/btnvtool', disabling 'config_bt_addr': No such file or directory

--- device.mk
+++ device.mk
@@ -130,8 +130,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
