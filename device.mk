fe63c0a925e74585b97035cc02e9280f3d472fa5
Razziell <razziell@ya.ru>
2018-07-02 17:43:34 +0300

    Fix for bt

--- device.mk
+++ device.mk
@@ -130,7 +130,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-service
+    android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth@1.0-impl
 
 # Camera
 PRODUCT_PACKAGES += \
