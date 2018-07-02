359cf2d95555ce97b2e7c6e8e295d1e4af34b71a
Razziell <razziell@ya.ru>
2018-07-02 12:41:32 +0300

    Move thermal config to device tree

--- device.mk
+++ device.mk
@@ -351,6 +351,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
