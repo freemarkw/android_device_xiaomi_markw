4e0e232394aae0fdb0db700beb824c3c9b161dbe
Razziell <razziell@ya.ru>
2018-07-07 22:32:13 +0300

    partly revert "markw: add thermal HALL support, add thermal config to device tree"

--- device.mk
+++ device.mk
@@ -359,6 +359,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 PRODUCT_PACKAGES += \
+    thermal.msm8953
+
+PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
