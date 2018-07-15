40bd0d3191530d97408d69c5cedb8ce149f81ece
Mikhail Kshevetskiy <mikhail.kshevetskiy@gmail.com>
2018-07-15 09:01:31 +0300

    markw: Remove implicitly built QTI camera shared libraries again

--- device.mk
+++ device.mk
@@ -137,8 +137,6 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
     mm-qcamera-app \
     camera.msm8953 \
     libmm-qcamera \
