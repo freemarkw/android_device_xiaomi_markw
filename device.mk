94c1bc1392eff0d6d1f997045e4b3c4670413f70
Razziell <razziell@ya.ru>
2018-08-28 23:33:23 +0300

    Use GPS from bardock-pro - OPM1.171019.019

--- device.mk
+++ device.mk
@@ -202,7 +202,6 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti \
     libcurl \
     libgnss \
     libgnsspps
