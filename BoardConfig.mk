47ca29f69609c331c4b8555a6955ce7b92c9fa8c
Mikhail Kshevetskiy <mikhail.kshevetskiy@gmail.com>
2018-09-24 23:25:29 +0300

    remove fstab_AB.qcom and it's traces

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,11 +203,7 @@ TARGET_USE_SDCLANG := true
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-ifeq ($(AB_OTA_UPDATER), true)
-TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab_AB.qcom
-else
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
-endif
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
