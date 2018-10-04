6b9bf94846326069c1d5388dcf30dc7361794128
Mikhail Kshevetskiy <mikhail.kshevetskiy@gmail.com>
2018-10-04 03:56:44 +0300

    update build description & fingerprint to V9.6.2.0.MBEMIFD

--- lineage.mk
+++ lineage.mk
@@ -26,6 +26,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=markw \
-    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V9.6.1.0.MBEMIFA release-keys"
+    PRIVATE_BUILD_DESC="markw-user 6.0.1 MMB29M V9.6.2.0.MBEMIFD release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V9.6.1.0.MBEMIFA:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/markw/markw:6.0.1/MMB29M/V9.6.2.0.MBEMIFD:user/release-keys
