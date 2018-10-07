e7291bb0397268df515e2d6b508efac962331fd5
Razziell <razziell@ya.ru>
2018-10-07 21:50:15 +0300

    libaudioampl: small clean

--- libaudioampl/Android.mk
+++ libaudioampl/Android.mk
@@ -1,21 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += \
-     $(call project-path-for,qcom-audio)/hal/msm8916/ \
-		 $(call project-path-for,qcom-audio)/hal/ \
-		 $(call project-path-for,qcom-audio)/hal/audio_extn \
-     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
-		 external/tinyalsa/include \
-		 external/tinycompress/include \
-		 hardware/libhardware/include \
-		 system/media/audio_route/include \
-		 device/xiaomi/markw/power \
+	$(call project-path-for,qcom-audio)/hal/msm8916/ \
+	$(call project-path-for,qcom-audio)/hal/ \
+	$(call project-path-for,qcom-audio)/hal/audio_extn \
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+	external/tinyalsa/include \
+	external/tinycompress/include \
+	hardware/libhardware/include \
+	system/media/audio_route/include \
 LOCAL_ADDITIONAL_DEPENDENCIES += INSTALLED_KERNEL_HEADERS
 LOCAL_SHARED_LIBRARIES := liblog \
- libutils \
- libcutils \
- libtinyalsa \
- libtinycompress
+	libutils \
+	libcutils \
+	libtinyalsa \
+	libtinycompress
 LOCAL_SRC_FILES := audio_amplifier.c
 LOCAL_MODULE := audio_amplifier.msm8953
 LOCAL_MODULE_RELATIVE_PATH := hw
