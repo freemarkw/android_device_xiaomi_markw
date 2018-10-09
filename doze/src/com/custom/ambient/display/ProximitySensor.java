7a26e624eda118f03ef4466cd827e3c577f6e836
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-10-09 22:08:39 +0300

    markw: doze: Fix proximity sensor

--- doze/src/com/custom/ambient/display/ProximitySensor.java
+++ doze/src/com/custom/ambient/display/ProximitySensor.java
@@ -51,7 +51,7 @@ public class ProximitySensor implements SensorEventListener {
     public ProximitySensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
