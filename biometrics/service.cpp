17361d83ebfa0b5af61ccfc6b30d49182c879ee5
Razziell <razziell@ya.ru>
2019-03-15 11:13:11 +0300

    biometrics: adjustments

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.biometrics.fingerprint@2.0-service.xiaomi_markw"
+#define LOG_TAG "android.biometrics.fingerprint@2.1-service.xiaomi_markw"
 
 #include <binder/ProcessState.h>
 
--- device.mk
+++ device.mk
@@ -177,7 +177,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service.xiaomi_markw
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_markw
 
 # FM
 PRODUCT_PACKAGES += \
