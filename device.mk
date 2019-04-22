170fd1b0f2828dda8c88f3e3b48c8b02a303cb60
Razziell <razziell@ya.ru>
2019-04-22 11:30:53 +0300

    device: disable RRO overlay

--- device.mk
+++ device.mk
@@ -24,9 +24,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    Bluetooth \
-    framework-res
+#PRODUCT_ENFORCE_RRO_TARGETS := \
+#    Bluetooth \
+#    framework-res
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
