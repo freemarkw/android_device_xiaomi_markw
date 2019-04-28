00a2af39713f9ad19511ed9b895158571265bab8
Alexander Shumilov <razziell@ya.ru>
2019-04-28 16:03:13 +0300

    Update prop.mk
    should fix some game hangs

--- prop.mk
+++ prop.mk
@@ -2,11 +2,6 @@
 # system.prop for markw
 #
 
-# ART
-PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.dex2oat-filter=speed \
-dalvik.vm.image-dex2oat-filter=speed
-
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
